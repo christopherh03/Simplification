e805c106e0bd33f5c31b8ae951f31d787af53e37
Christopher Hampton <williamhampton7@gmail.com>
2017-12-24 08:50:09 -0500

    Create Twitter-Search.py

---
+++ python/Twitter-Search.py
@@ -0,0 +1,7 @@
+## Author: Christopher Hampton
+import os
+
+Pre_Query = input("What do you want to search on Youtube?: ")
+Query = Pre_Query.replace(' ', '+')
+URL = 'start https://twitter.com/search?q=' + Query
+os.system(URL)
